3b9b75f015b483a7c483ade89c8ef46a521551f6
3dragons <3dragons@dragons.com>
2024-03-20 18:25:36 +0800

    01

---
+++ src/main/java/tk/tools/RenderBox.java
@@ -0,0 +1,118 @@
+package tk.tools;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.Color;
+
+@SuppressWarnings("unused")
+public class RenderBox {
+    private final static Minecraft mc = Minecraft.getMinecraft();
+
+    // 还原GL状态
+    private static void resetGL() {
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopMatrix();
+    }
+
+    private static void renderLine(final double x1, final double y1, final double z1, final double x2, final double y2, final double z2) {
+        GL11.glVertex3d(x1, y1, z1);
+        GL11.glVertex3d(x2, y2, z2);
+    }
+
+    public String renderBox(final double maxX, final double maxY, final double maxZ, final double minX, final double minY, final double minZ, final Color color) {
+        try {
+            // 保存GL状态
+            GL11.glPushMatrix();
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glEnable(GL11.GL_LINE_SMOOTH);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBegin(GL11.GL_LINES);
+
+            // 设置颜色和宽度
+            final float red = color.getRed() / 255f;
+            final float green = color.getGreen() / 255f;
+            final float blue = color.getBlue() / 255f;
+            final float alpha = color.getAlpha() / 255f;
+            GlStateManager.color(red, green, blue, alpha);
+            GL11.glLineWidth(2);
+
+            // 渲染上方矩形
+            renderLine(minX, minY, minZ, maxX, minY, minZ);
+            renderLine(maxX, minY, minZ, maxX, minY, maxZ);
+            renderLine(maxX, minY, maxZ, minX, minY, maxZ);
+            renderLine(minX, minY, maxZ, minX, minY, minZ);
+
+            // 渲染下方矩形
+            renderLine(minX, maxY, minZ, maxX, maxY, minZ);
+            renderLine(maxX, maxY, minZ, maxX, maxY, maxZ);
+            renderLine(maxX, maxY, maxZ, minX, maxY, maxZ);
+            renderLine(minX, maxY, maxZ, minX, maxY, minZ);
+
+            // 渲染连接矩形
+            renderLine(minX, minY, minZ, minX, maxY, minZ);
+            renderLine(maxX, minY, minZ, maxX, maxY, minZ);
+            renderLine(maxX, minY, maxZ, maxX, maxY, maxZ);
+            renderLine(minX, minY, maxZ, minX, maxY, maxZ);
+            GL11.glEnd();
+        } catch (Error error) {
+            resetGL();
+            return error.getMessage();
+        }
+        resetGL();
+        return "Success";
+    }
+
+    public String renderBox(double maxX, double maxY, double maxZ, double minX, double minY, double minZ) {
+        return renderBox(maxX, maxY, maxZ, minX, minY, minZ, Color.GREEN);
+    }
+
+    // 重载 通过AxisAlignedBB传递参数
+    public String renderBox(AxisAlignedBB box, Color color) {
+        return renderBox(box.maxX, box.maxY, box.maxZ, box.minX, box.minY, box.minZ, color);
+    }
+
+    public String renderBox(AxisAlignedBB box) {
+        return renderBox(box.maxX, box.maxY, box.maxZ, box.minX, box.minY, box.minZ, Color.GREEN);
+    }
+
+    // 渲染实体
+    public String renderEntity(final Entity entity, Color color) {
+        // 读取实体坐标
+        final AxisAlignedBB box = entity.getEntityBoundingBox();
+        double maxX = box.maxX;
+        double maxY = box.maxY;
+        double maxZ = box.maxZ;
+        double minX = box.minX;
+        double minY = box.minY;
+        double minZ = box.minZ;
+
+        // 计算相对位置
+        final RenderManager renderManager = RenderBox.mc.getRenderManager();
+        final double playerViewX = renderManager.viewerPosX;
+        final double playerViewY = renderManager.viewerPosY;
+        final double playerViewZ = renderManager.viewerPosZ;
+        maxX = maxX - playerViewX;
+        maxY = maxY - playerViewY;
+        maxZ = maxZ - playerViewZ;
+        minX = minX - playerViewX;
+        minY = minY - playerViewY;
+        minZ = minZ - playerViewZ;
+        return renderBox(maxX, maxY, maxZ, minX, minY, minZ, color);
+    }
+
+    public String renderEntity(Entity entity) {
+        return renderEntity(entity, Color.GREEN);
+    }
+}
---
+++ src/main/java/tk/tools/RenderLine3D.java
@@ -0,0 +1,113 @@
+package tk.tools;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.BlockPos;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.OpenGLException;
+
+import java.awt.Color;
+
+@SuppressWarnings("unused")
+public class RenderLine3D {
+
+    final private static Minecraft mc = Minecraft.getMinecraft();
+
+    private static double[] getRelativePosition(double x, double y, double z) {
+        // 读取渲染位置
+        final RenderManager renderManager = mc.getRenderManager();
+        final double viewerPosX = renderManager.viewerPosX;
+        final double viewerPosY = renderManager.viewerPosY;
+        final double viewerPosZ = renderManager.viewerPosZ;
+
+        // 计算相对位置
+        final double relativeX = x - viewerPosX;
+        final double relativeY = y - viewerPosY;
+        final double relativeZ = z - viewerPosZ;
+
+        return new double[]{relativeX, relativeY, relativeZ};
+    }
+
+    final public String renderLine3D(double x1, double y1, double z1, double x2, double y2, double z2, final Color color, final boolean global) {
+        try {
+            // 保存GL状态
+            GL11.glPushMatrix();
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glEnable(GL11.GL_LINE_SMOOTH);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBegin(GL11.GL_LINES);
+
+            // 设置颜色和宽度
+            final float red = color.getRed() / 255f;
+            final float green = color.getGreen() / 255f;
+            final float blue = color.getBlue() / 255f;
+            final float alpha = color.getAlpha() / 255f;
+            GlStateManager.color(red, green, blue, alpha);
+            GL11.glLineWidth(2);
+
+            // 计算相对位置
+            final double[] pos1 = global ? getRelativePosition(x1, y1, z1) : new double[]{x1, y1, z1};
+            x1 = pos1[0];
+            y1 = pos1[1];
+            z1 = pos1[2];
+            final double[] pos2 = global ? getRelativePosition(x2, y2, z2) : new double[]{x2, y2, z2};
+            x2 = pos2[0];
+            y2 = pos2[1];
+            z2 = pos2[2];
+
+            // 渲染
+            GL11.glVertex3d(x1, y1, z1);
+            GL11.glVertex3d(x2, y2, z2);
+            GL11.glEnd();
+
+            // 还原GL状态
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_LINE_SMOOTH);
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glPopMatrix();
+        } catch (OpenGLException error) {
+            return error.getMessage();
+        } catch (Exception error) {
+            return error.getMessage();
+        }
+        return "Success";
+    }
+
+    // 重载 完整传递参数但是坐标用BlockPos类型
+    final public String renderLine3D(BlockPos pos1, BlockPos pos2, Color color, boolean global) {
+        return renderLine3D(pos1.getX(), pos1.getY(), pos1.getZ(), pos2.getX(), pos2.getY(), pos2.getZ(), color, global);
+    }
+
+    // 重载 仅需传递坐标
+    final public String renderLine3D(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return renderLine3D(x1, y1, z1, x2, y2, z2, Color.GREEN, true);
+    }
+
+    final public String renderLine3D(BlockPos pos1, BlockPos pos2) {
+        return renderLine3D(pos1, pos2, Color.GREEN, true);
+    }
+
+    // 重载 需要传递颜色
+    final public String renderLine3D(double x1, double y1, double z1, double x2, double y2, double z2, Color color) {
+        return renderLine3D(x1, y1, z1, x2, y2, z2, color, true);
+    }
+
+    final public String renderLine3D(BlockPos pos1, BlockPos pos2, Color color) {
+        return renderLine3D(pos1, pos2, color, true);
+    }
+
+    // 重载 需要传递是否全局
+    final public String renderLine3D(double x1, double y1, double z1, double x2, double y2, double z2, boolean global) {
+        return renderLine3D(x1, y1, z1, x2, y2, z2, Color.GREEN, global);
+    }
+
+    final public String renderLine3D(BlockPos pos1, BlockPos pos2, boolean global) {
+        return renderLine3D(pos1, pos2, Color.GREEN, global);
+    }
+}
